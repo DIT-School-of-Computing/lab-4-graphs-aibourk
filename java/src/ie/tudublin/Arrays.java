5456efda82ca97022cb7165c6e4104c022a3cb53
Amy <124152568+aibourk@users.noreply.github.com>
2024-02-18 22:30:02 +0000

    Added the bars and colour

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -102,35 +102,46 @@ public class Arrays extends PApplet
 	
 	public void draw()
 	{	
+		float w = width * 0.8f; //400 / total width/length of chart
 		switch (mode) {
 			case 0:
 				background(0);
 				textSize(15);
                 text("Click different numbers for different graphs", 140, 250);
 				break;
-			case 1:
-				background(0);
-				float w = width * 0.8f; //400 / total width/length of chart
-			
+			case 1: //bar chart
+				background(0);			
 				fill(360, 0, 100);
 				stroke(360, 0, 100);
+
+				//the axis
 				line(width * 0.1f, width * 0.1f, width * 0.1f, width - (width * 0.1f)); // 50,50,50,450
 				line(width * 0.1f, width - (width * 0.1f), width - (width * 0.1f), width - (width * 0.1f)); //50,450,450,450
 				
+				//the ticks and labels
 				for(int i = 50; i < width-(w * 0.1); i = i + (int)w/10)
 				{
 					line(width * 0.1f, i, (width * 0.1f) - 5, i);
 					text(((i-50)/40) * 50, (width * 0.02f), width - i );
 				}
-				
 				for	(int i = 0; i < months.length; i++)
 				{
 					text(months[i], 50+(i*35) , 470);
 				}
 
+				//the bars of the barchart
+				for(int i = 0; i < months.length; i++)
+				{
+					int start = 50;
+					float widthOfColumn = w / (float)months.length;
+					fill(i*20, 100, 360);
+					rect(start + (i * widthOfColumn), width-start, widthOfColumn, -((rainfall[i]/500) * 400));
+				}
+
 				break;
-			case 2:
-				background(0);
+			case 2: //line graph
+				background(0);			
+				
 				break;
 		}
 	}
